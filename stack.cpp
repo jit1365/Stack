fc9a7467b9092dd0743b53e18cec069f2b6da090
Deep22k <116004266+Deep22k@users.noreply.github.com>
2022-10-17 20:05:17 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+//#include<stack>
+using namespace std;
+
+class stack{
+    public:
+    int top;
+    int* arr;
+    int size;
+
+    stack(int size){
+        this->size=size;
+        arr = new int[size];
+        top=-1;
+    }
+
+    void push(int element){
+        if(top<size-1){
+            top++;
+            arr[top]=element;
+        }
+        else{
+            cout<<"Stack  OverFlow"<<endl;
+        }
+    }
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout<<"Stack  UnderFlow"<<endl;
+
+        }
+    }
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }
+        else{
+            cout<<"Stack is Empty."<<endl;
+            return -1;
+        }
+    }
+    bool isEmpty(){
+        if(top== -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+int main(){
+    stack s(3);
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    if(s.isEmpty()){
+        cout<<"Stack is Empty."<<endl;
+    }
+
+
+
+
+    /*
+    //creation of stack
+    stack<int> s;
+    //push operation
+    s.push(5);
+    s.push(8);
+    //pop
+    s.pop();
+    cout<<s.top(); 
+    if(s.empty()){
+        cout<<"Stack is empty."<<endl;
+    }
+    else{
+        cout<<"Stack is not empty."
+    }
+    */
+
+    return 0;
+}
---
+++ stack_using_LL.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int  data;
+    node *next;
+
+    node(){
+        this->next=NULL;
+    }
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+    ~node(){
+        int value=this->data;
+        if(this->next!=NULL){
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"memory is free for node with data "<<value<<endl;
+    }
+    node* top=NULL;
+
+    void push(int element){
+        node* n = new node(element);
+        if (!n){
+        cout << "\nStack Overflow";
+        return;
+        }
+        else{
+            n->next=top;
+            top=n;
+        }
+    }
+    void pop(){
+        if(top==NULL){
+            cout<<"Stack UnderFlow"<<endl;
+            return;
+        }
+        else{
+            node* toDelete=top;
+            top=top->next;
+            toDelete->next=NULL;
+            delete toDelete;
+        }
+    }
+    int peek(){
+        if(top==NULL){
+            cout<<"Stack is Empty."<<endl;
+            return -1;
+        }
+        else{
+            return top->data;
+        }
+    }
+    bool isEmpty(){
+        if(top==NULL){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+};
+
+int main(){
+    node s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    cout<<s.peek()<<endl;
+    s.pop();
+    if(s.isEmpty()){
+        cout<<"Stack is Empty."<<endl;
+    }
+}
